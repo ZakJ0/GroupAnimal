9c8c01925bc50e2d3cc4a55d93ecf8a813f0ad5b
Estrid4Ever <johannes-sr@hotmail.com>
2024-01-26 13:56:38 +0100

    file restructure

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import se.GroupAnimal.*;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args){
-        Animal Cat= new Cat("Måns",12,"banan",true,30000,"Robert");
-        Animal Elphant= new Elefant("yo");
-        Person p1=new Personne("Anders",20);
-        Insurable in= new Insurrance(2,"890000","Zack",100);
+    public static void main(String[] args) {
+        Animal Cat = new Cat("Måns", 12, "banan", true, 30000, "Robert");
+        Animal Elphant = new Elefant("yo");
+        Person p1 = new Personne("Anders", 20);
+        Insurable in = new Insurrance(2, "890000", "Zack", 100);
 
         speed(Cat);
         speed(Elphant);
@@ -23,21 +25,26 @@ public class Main {
         Insurable toaster = new Toaster("filips", 300, 5);
         printInsuranceCost(toaster);
     }
-    public static void pay(Insurable in){
+
+    public static void pay(Insurable in) {
         in.calculateMonthlyPayment();
     }
-    public static void speed(Animal a){
+
+    public static void speed(Animal a) {
         a.run();
     }
-    public static void greetings(Person p1){
+
+    public static void greetings(Person p1) {
         p1.greet();
     }
-    public static void printInsuranceCost(Insurable insurable){
+
+    public static void printInsuranceCost(Insurable insurable) {
         System.out.println("Monthly cost: " + insurable.calculateMonthlyPayment()
                 + " kr\nDeductable: " + insurable.calcualteDeductable() + " kr");
     }
-    public static void printAnimalSounds(List<Animal> animals){
-        for (Animal a : animals){
+
+    public static void printAnimalSounds(ArrayList<Animal> animals) {
+        for (Animal a : animals) {
             a.makeSound();
         }
     }
